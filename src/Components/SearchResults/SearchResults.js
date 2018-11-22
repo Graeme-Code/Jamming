9f4739194c4b9ff601e37e45ec4304cc8c0c3fd3
Graeme-Code <graemebarnes1987@gmail.com>
2018-11-22 21:07:47 +0000

    all code with this section of project

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SearchResults.css';
+//import Trachlist componenet
+import { TrackList } from '../TrackList/TrackList.js'
+
+class SearchResults extends React.Component {
+  render () {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+      </div>
+    );
+  }
+}
